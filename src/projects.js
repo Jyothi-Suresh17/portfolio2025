2a5347effc764f99a6deaf620154cdbbdd15b365
Jyothi Suresh <jyothisuresh2521@gmail.com>
2025-04-01 14:33:11 +0530

    modify thumbnail path

--- src/projects.js
+++ src/projects.js
@@ -3,28 +3,28 @@ export const projects = [
     id: 1,
     title: "BuzzChat",
     description: "A real-time chat application built using React and Socket.io.",
-    image: "/src/assets/projectThum/BuzzChat.png",
+    image: "/src/assets/BuzzChat.png",
     github: "https://github.com/Jyothi-Suresh17/BuzzChat"
   },
   {
     id: 2,
     title: "LearnUm",
     description: "An online learning platform built with React and Tailwind CSS.",
-    image: "/src/assets/projectThum/LearnUm.png",
+    image: "/src/assets/LearnUm.png",
     github: "https://github.com/Jyothi-Suresh17/LearnumFrontend"
   },
   {
     id: 3,
     title: "FlickPicks",
     description: "A movie recommendation app powered by the TMDB API and React.",
-    image: "/src/assets/projectThum/FlickPicks.png",  
+    image: "/src/assets/FlickPicks.png",  
     github: "https://github.com/Jyothi-Suresh17/FlickPicks"
   },
   {
     id: 4,
     title: "Employee Task Manager",
     description: "A task manager for employees, built with React and Node.js.",
-    image: "/src/assets/projectThum/employeeManager.png",
+    image: "/src/assets/employeeManager.png",
     github: "https://github.com/Jyothi-Suresh17/EmployeeManeger"
   }
 ];
